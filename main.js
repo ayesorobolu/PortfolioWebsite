dff72435e9ab808ddbccc868b14283e76d91a42b
Ayesoro Boluwatife <abraham@Ayesoros-MacBook-Pro.local>
2024-10-14 12:04:59 +0100

    Prototype design

--- main.js
+++ main.js
@@ -15,3 +15,15 @@ navLinks.forEach(link => {
         bar.classList.remove('toggle');
     });
 });
+
+const links = document.querySelectorAll('nav a');
+
+links.forEach(link => {
+    link.addEventListener('click', function() {
+        // Remove 'active' class from all links
+        links.forEach(link => link.classList.remove('active'));
+
+        // Add 'active' class to the clicked link
+        this.classList.add('active');
+    });
+});
