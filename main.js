0a23bc24b5e4262ed8ff1fc56a2d46cabcfecb0b
Ayesoro Boluwatife <abraham@Ayesoros-MacBook-Pro.local>
2024-10-17 12:50:43 +0100

    All sections added with media query

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const bar = document.querySelector('.bar');
 const nav = document.querySelector('.nav-links');
 const navLinks = document.querySelectorAll('.nav-links a');
@@ -27,3 +28,36 @@ links.forEach(link => {
         this.classList.add('active');
     });
 });
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec =>{
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height){
+navlinks.forEach(links => {
+links.classList.remove('active');
+document.querySelector('header nav a[href*= '+ id + ']').classList.add('active');
+});
+    };
+  });
+};
+
+const observer = new IntersectionObserver((entries)  => {
+  entries.forEach ((entry)=> {
+    console.log(entry)
+    if (entry.isIntersecting){
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show')
+    }
+  }) ;
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
